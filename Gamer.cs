6d59ed1ad20d45b23f05b548fdee59d85b1e7cd9
tatiana postolovskaya <tanusa1910@live.com>
2024-02-16 10:01:35 +0300

    create class Gamer.

---
+++ Gamer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Gamer
+{
+	string name;
+	int capital;
+	public string Name
+	{
+		get { return name; }
+		set { name = value; }
+	}
+	public int Capital
+	{
+		get { return capital; }
+		set { capital = value; }
+	}
+	public void UpgrateCapital(int value)
+	{
+		capital += value;
+	}
+}
